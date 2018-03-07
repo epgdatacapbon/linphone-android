f16c6e9a44892e8358af7ec63c6706042ea22a6a
epgdatacapbon <sstream00@yahoo.co.jp>
2018-03-07 22:46:10 +0900

    Implement push notification

--- src/android/org/linphone/firebase/FirebaseMessaging.java
+++ src/android/org/linphone/firebase/FirebaseMessaging.java
@@ -19,18 +19,31 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.PowerManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+
+import org.linphone.compatibility.Compatibility;
+import org.linphone.LinphoneLauncherActivity;
 import org.linphone.LinphoneManager;
 import org.linphone.LinphoneService;
+import org.linphone.R;
 import org.linphone.UIThreadDispatcher;
-import static android.content.Intent.ACTION_MAIN;
 
 
 public class FirebaseMessaging extends FirebaseMessagingService {
+    private final static int WAKE_TIME = 10000;
+    private final static int INCALL_NOTIF_ID=2;
+
     public FirebaseMessaging() {
     }
 
@@ -38,8 +51,10 @@ public class FirebaseMessaging extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         android.util.Log.i("FirebaseMessaging","[Push Notification] Received");
 
+        acquireWakeLock(WAKE_TIME);
+
         if (!LinphoneService.isReady()) {
-            startService(new Intent(ACTION_MAIN).setClass(this, LinphoneService.class));
+            startService(new Intent(Intent.ACTION_MAIN).setClass(this, LinphoneService.class));
         } else if (LinphoneManager.isInstanciated() && LinphoneManager.getLc().getCallsNb() == 0) {
             UIThreadDispatcher.dispatch(new Runnable(){
                 @Override
@@ -51,6 +66,37 @@ public class FirebaseMessaging extends FirebaseMessagingService {
                 }
             });
         }
+
+        sendNotification(remoteMessage.getData());
     }
 
+    void acquireWakeLock(long timeout) {
+        PowerManager mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock mWakeLock = mPowerManager.newWakeLock(
+                PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.PARTIAL_WAKE_LOCK,
+                "FirebaseMessaging");
+        mWakeLock.acquire(timeout);
+    }
+
+    void sendNotification(Map<String, String> data) {
+        final String msg = data.get("msg");
+        final String num = data.get("num");
+
+        NotificationManager mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNM.cancel(INCALL_NOTIF_ID); // in case of crash the icon is not removed
+        Compatibility.CreateChannel(this);
+
+        Intent notifIntent = new Intent(Intent.ACTION_CALL)
+                .setData(Uri.parse("tel:" + num)).setClass(this, LinphoneLauncherActivity.class);
+        PendingIntent mNotifContentIntent = PendingIntent.getActivity(
+                this, 0, notifIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        
+        Notification mNotif = Compatibility.createNotification(
+                this, getString(R.string.incoming), msg, R.drawable.linphone_notification_icon,
+                R.mipmap.ic_launcher, null, mNotifContentIntent, true, Notification.PRIORITY_HIGH);
+
+        mNotif.defaults |= Notification.DEFAULT_SOUND | Notification.DEFAULT_LIGHTS;
+
+        mNM.notify(INCALL_NOTIF_ID, mNotif);
+    }
 }
